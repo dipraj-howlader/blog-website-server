76c0e6d48699e56bfd0165e83a03c74159ddb8fc
Dipraj Howlader <diprajhowlader2015@gmail.com>
2021-06-24 21:24:03 +0600

    env works

--- index.js
+++ index.js
@@ -2,11 +2,18 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
+require('dotenv').config();
+const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
+
+
 
 const port = 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
+
+console.log(process.env.DB_PASS);
 
 app.get('/',(req,res) => {
     res.send('Hello World')

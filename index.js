20491636730e4c01cecfe21693709cc2a3a89822
Dipraj Howlader <diprajhowlader2015@gmail.com>
2021-06-24 22:03:26 +0600

    mongodb working

--- index.js
+++ index.js
@@ -13,11 +13,30 @@ const port = 5000;
 app.use(cors());
 app.use(bodyParser.json());
 
-console.log(process.env.DB_PASS);
 
 app.get('/',(req,res) => {
     res.send('Hello World')
 })
+// console.log(process.env.DB_PASS)
 
+//blogs
+const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.wthiz.mongodb.net/BlogWebsite?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const blogsCollection = client.db("BlogWebsite").collection("blog");
+
+  //blogs
+
+  app.post('/addBlog',(req, res) =>{
+    const newBlog = req.body;
+    
+    blogsCollection.insertOne(newBlog)
+    .then(result => {
+        console.log('inserted ', result.insertedCount)
+        res.send(res.insertedCount > 0)
+    })
+})
+  
+});
 
 app.listen(process.env.PORT || port)
